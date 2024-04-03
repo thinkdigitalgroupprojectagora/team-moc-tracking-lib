0c599c4d94af549a841e13ec2c022d8627bf56f4
Damianos Damianidis <cloud.tailwind@gmail.com>
2024-04-03 09:31:54 +0300

    "Flask>=2.0.0"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tracking_lib",
-    version="0.1.26",
+    version="0.1.27",
     url="https://github.com/thinkdigitalgroupprojectagora/team-moc-tracking-lib",
     author="Damianos Damianidis",
     author_email="damianosd@projectagora.com",
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "fastapi>=0.103.1",
-        "Flask>=1.1.4",
+        "Flask>=2.0.0",
         "google-cloud-pubsub>=2.18.4",
     ],
 )
